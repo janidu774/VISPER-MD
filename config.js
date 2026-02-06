4f6d2f90678935e2bccd88f0b0ca940dcefb0272
janidu774 <150676032+janidu774@users.noreply.github.com>
2026-02-06 21:55:08 +0530

    Update default SESSION_ID in config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'PUT SESSION ID HERE' : process.env.SESSION_ID, 
+SESSION_ID: process.env.SESSION_ID === undefined ? 'VISPER-MD&f80c077c-1fc8-41e6-8dc0-4793e61ba87a' : process.env.SESSION_ID, 
 PORT: process.env.PORT === undefined ? '8000' : process.env.PORT,    
 SESSION_NAME: process.env.SESSION_NAME === undefined ? 'vispermd' : process.env.SESSION_NAME, 
 
